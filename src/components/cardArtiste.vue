1bbaaadb40e58bcb052e23a9126bf34badb70f41
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-30 21:16:50 +0200

    creation du composant cardArtiste

---
+++ src/components/cardArtiste.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="relative w-72 h-52 md:w-96 md:h-64 border-4 border-white border-dashed">
+        <img class="h-full object-cover" :src=image :alt=imgalt>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+ name: "card",
+ props:{
+     image:{
+         type: String,
+     },
+     imgalt:{
+         type: String,
+     },
+ }
+}
+
+</script>
