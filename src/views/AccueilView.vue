0eff086b4ff142b8dcb0671af72f06a7a567d877
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-30 14:25:51 +0200

    accueil - 1ere partie

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,9 +1,21 @@
 <template>
-  <main >
-    <h1 class="text-2xl">Bonjour</h1>
-  </main>
+  <div>
+    <div class="flex justify-center pt-32 pb-14 bg-pink-300">
+      <logoGrand class="mx-10 max-w-4xl"/>
+      <span class="sr-only">Rock en Stras'</span>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import logoGrand from "../components/icons/logoGrand.vue"
+
+export default {
+  name: "AccueilView",
+  data() {
+    return {
+    };
+  },
+  components: { logoGrand },
+}
 </script>
