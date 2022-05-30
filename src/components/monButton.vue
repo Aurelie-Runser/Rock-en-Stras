e95500b5fd5fc5a11bb201ef883e40ea0b7460b8
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-30 16:56:30 +0200

    creation du bouton

---
+++ src/components/monButton.vue
@@ -0,0 +1,6 @@
+<template>
+    <button class="py-2 px-5 w-max bg-pink-300_90 border-b-pink-800 border-r-pink-800 border-b-4 border-r-4 rounded-r-3xl rounded-l-md
+                    hover:bg-black hover:border-b-gray-300 hover:border-r-gray-300">
+        <slot/>
+    </button>
+</template>
