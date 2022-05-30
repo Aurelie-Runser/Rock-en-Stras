b17f7576959102d0dba48c573ad10e6a96f41dfc
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-31 00:50:59 +0200

    changement de display du boutton, passage en block

--- src/components/monButton.vue
+++ src/components/monButton.vue
@@ -1,7 +1,8 @@
 <template>
-    <button class="m-5 mt-12 py-2 px-5 w-max bg-pink-300_90 border-b-pink-800 border-r-pink-800 border-b-4 border-r-4 rounded-r-3xl rounded-l-md
+    <button class="block m-5 mt-12 py-2 px-5 w-max bg-pink-300_90 border-b-pink-800 border-r-pink-800 border-b-4 border-r-4 rounded-r-3xl rounded-l-md
                     hover:bg-black hover:border-b-gray-300 hover:border-r-gray-300
-                    focus:bg-black focus:border-b-gray-300 focus:border-r-gray-300">
-        <p class="font-button text-lg mx-auto"><slot/></p>
+                    focus:bg-black focus:border-b-gray-300 focus:border-r-gray-300
+                    text-lg">
+        <p class="font-button mx-auto"><slot/></p>
     </button>
 </template>
