0281902ee0ed2ba439ac2ae629c3e720640aeb5f
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-31 01:20:51 +0200

    modification de l'header : fond unis, alignement des pages à droite

--- src/components/monheader.vue
+++ src/components/monheader.vue
@@ -1,5 +1,5 @@
 <template>
-        <nav class="z-40 w-full h-24 bg-gradient-to-r from-pink-300 to-transparent flex flex-row items-center justify-between px-5">
+        <nav class="z-40 w-full h-24 bg-pink-300 flex flex-row items-center justify-between px-5">
             <div>
                 <RouterLink to="/">
                     <logoPetit class="w-14"/>
@@ -18,12 +18,13 @@
                     <img v-else
                             src="../../public/menu/menuOuvert.png" alt="boutton pour fermer le menu">
 
-                    <span class="sr-only">Menu</span>
+                    <span class="sr-only">Bouton du menu</span>
                 </button>
             </div>
 
+            <!-- Menu phone -->
             <div id="menu"
-                class="fixed -translate-y-full px-5 inset-0 bg-gradient-to-b from-pink-300 to-black motion-safe:duration-500 motion-safe:transition-transform sm:hidden"
+                class="fixed -translate-y-full px-5 inset-0 bg-gradient-to-b from-pink-300 to-black motion-safe:duration-500 motion-safe:transition-transform md:hidden"
                 :class="{ 'translate-y-0': menuOuvert }">
 
                 <ul class="my-24 font-h2 text-2xl text-white leading-loose">
@@ -35,8 +36,8 @@
                 </ul>
             </div>
 
-            <ul class="hidden my-2 flex-auto justify-evenly content-between font-h3 text-xl text-white sm:flex">
-                <!-- Autre menu -->
+            <!-- Menu ordi -->
+            <ul class="hidden my-2 flex-auto justify-end font-h3 text-xl gap-10 text-white md:flex">
                 <li><RouterLink to="/program" :aria-expanded="menuOuvert" @click="menuOuvert = !menuOuvert">Programmation</RouterLink></li>
                 <li><RouterLink to="/artistes" :aria-expanded="menuOuvert" @click="menuOuvert = !menuOuvert">Artistes</RouterLink></li>
                 <li><RouterLink to="/festival" :aria-expanded="menuOuvert" @click="menuOuvert = !menuOuvert">Festival</RouterLink></li>
