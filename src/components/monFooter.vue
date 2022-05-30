a63b6a0b95ff9dc94f84fb76a2ac0c449f2ba778
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-30 12:19:05 +0200

    ajout du logo

--- src/components/monFooter.vue
+++ src/components/monFooter.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-        
-        foot
+    <div class="w-full p-7 bg-gray-300">
+        <div>
+            <RouterLink to="/">
+                <img src="../../public/logo/logoPetit.png" alt="logo">
+                <span class="sr-only">Accueil</span>
+            </RouterLink>
+        </div>
+
     </div>
 </template>
+
+<script>
+
+export default {
+  name: "monFooter",
+  data() {
+    return {
+    };
+  },
+  components: {  },
+}
+</script>
