56f3565a41bad7a63a9ecc98326a646bf6b0b455
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-30 13:02:11 +0200

    footer responsive

--- src/components/monFooter.vue
+++ src/components/monFooter.vue
@@ -1,16 +1,30 @@
 <template>
-    <div class="w-full p-7 bg-gray-300">
-        <div>
-            <RouterLink to="/">
-                <img src="../../public/logo/logoPetit.png" alt="logo">
-                <span class="sr-only">Accueil</span>
-            </RouterLink>
+    <div class="w-full p-7 bg-gray-300 text-pink-500 font-base">
+        <div class="flex flex-wrap justify-between content-around gap-8">
+            <div>
+                <RouterLink to="/">
+                    <img class="w-14" src="../../public/logo/logoPetit.png" alt="logo">
+                    <span class="sr-only">Accueil</span>
+                </RouterLink>
+            </div>
+
+            <div class="flex gap-4 flex-none">
+                <img class="w-12 h-12" src="../../public/reseau/facebookJour.png" alt="lien vers facebook">
+                <img class="w-12 h-12" src="../../public/reseau/instagramJour.png" alt="lien vers instagram">
+                <img class="w-12 h-12" src="../../public/reseau/twitterJour.png" alt="lien vers twitter">
+            </div>
+
+            <div>
+                <p>Nous contacter</p>
+            </div>
+
+            <div>
+                <p>Mentions légales</p>
+            </div>
         </div>
 
-        <div class="flex gap-4">
-            <img src="../../public/reseau/facebookJour.png" alt="lien vers facebook">
-            <img src="../../public/reseau/instagramJour.png" alt="lien vers instagram">
-            <img src="../../public/reseau/twitterJour.png" alt="lien vers twitter">
+        <div class="mt-6 text-center">
+            <p>© MMI Montbéliard - 2022</p>
         </div>
     </div>
 </template>
