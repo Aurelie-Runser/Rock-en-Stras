2c4faf268154d1dc68288c980f22098fcd45617a
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-31 08:37:05 +0200

    creation page de contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,73 @@
 <template>
-    <div>
-        contact
+    <div class="bg-gray-900 text-white px-10 py-36">
+        <div class="">
+            <h1>Contact</h1>
+            <img class="hidden sm:inline-block"
+                 src="../../public/lettre.png" 
+                 alt="dessin d'une enveloppe">
+
+            <p>Des détails pas claires ? Une information manquante ? Un problème ?</p>
+            <p>Écrirez-nous un message !</p>
+        </div>
+        <form>
+            <div>
+                <label for="name" class="sr-only">Nom Prénom (obligatoire)</label>
+                <input 
+                    type="text" 
+                    id="name" 
+                    name="user_name"
+                    placeholder="Nom Prénom *"
+                    required>
+            </div>
+        
+            <div>
+                <label for="name" class="sr-only">Adresse mail (obligatoire)</label>
+                <input 
+                    type="text" 
+                    id="mail" 
+                    name="user_mail"
+                    placeholder="Adresse mail *"
+                    required>
+            </div>
+        
+            <div>
+                <label for="object" class="sr-only">Objet (obligatoire)</label>
+                <input 
+                    type="text" 
+                    id="object" 
+                    name="user_object"
+                    placeholder="Objet *"
+                    required>
+            </div>
+        
+            <div>
+                <label for="message" class="sr-only">Message (obligatoire)</label>
+                <textarea 
+                    id="message" 
+                    name="user_message"
+                    placeholder="Message *"
+                    required>
+                </textarea>
+            </div>
+
+            <div>
+                <input 
+                    type="checkbox" 
+                    id="object" 
+                    name="user_object"
+                    placeholder="Objet *"
+                    required>
+                <label for="object" class="">J’ai lu, compris et accepte de partager ses informations et leur conditions d’utilisation.</label>   
+
+                <p>* champs obligatoires</p>
+            </div>
+
+            <div>
+                <input
+                    type="submit" 
+                    name="envoyer" 
+                    id="envoyer">
+            </div>
+        </form>
     </div>
 </template>
