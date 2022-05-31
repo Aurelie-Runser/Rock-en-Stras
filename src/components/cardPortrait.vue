7c605ea3fb6eb1b4f275fdd4c98db956eef9beda
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-31 14:17:58 +0200

    creation de la card portrait

---
+++ src/components/cardPortrait.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="relative w-36 h-52 md:w-48 md:h-64 xl:w-60 xl:h-80 border-4 border-white border-dashed">
+        <img class="h-full object-cover" :src=image :alt=imgalt>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+ name: "card",
+ props:{
+     image:{
+         type: String,
+     },
+     imgalt:{
+         type: String,
+     },
+ }
+}
+
+</script>
