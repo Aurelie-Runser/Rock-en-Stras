2d83276ed6699f83b7a54d8606f2259a33209c50
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-06-01 00:45:56 +0200

    utilisation de ratio plutot que differents height pour plus de coherence et de fluidité

--- src/components/cardPaysage.vue
+++ src/components/cardPaysage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="aspect-ratio w-72 md:w-96 xl:w-[30rem] border-4 border-gray-300">
+    <div class="aspect-video w-72 md:w-96 xl:w-[30rem] border-4 border-gray-300">
         <img class="w-full h-full object-cover" :src=image :alt=imgalt>
     </div>
 </template>
