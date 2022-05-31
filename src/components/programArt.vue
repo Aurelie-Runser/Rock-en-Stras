bc91742d136e6ee17bb0259cda45beae4bea0cdc
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-31 20:37:25 +0200

    ajustement des elements permettant le responsive

--- src/components/programArt.vue
+++ src/components/programArt.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="mx-auto my-16 w-full xs:w-[80%] lg:w-full flex flex-row justify-between items-center">
+    <div class="mx-auto my-16 w-full xs:w-[80%] lg:w-full flex flex-row gap-10 items-center">
         
         <cardPortrait class="lg:hidden"/>
         <cardPaysage class="hidden lg:block"/>
 
-        <div class="text-white font-base sm:mx-auto">
-            <h4 class="font-semibold text-base sm:text-lg m-5 sm:m-10">{{jour}}</h4>
+        <div class="text-white font-base mx-auto">
+            <h4 class="font-semibold text-base lg:text-lg mx-5 mb-5 sm:mx-10">{{jour}}</h4>
                         
-            <ul class="font-semibold font-base text-lg sm:text-xl leading-10 sm:leading-[4rem] nth-child-2n:text-pink-500">
+            <ul class="font-semibold text-base sm:text-lg lg:text-xl leading-10 sm:leading-[4rem] lg:leading-[5rem] nth-child-2n:text-pink-500">
                 <li>{{artiste1}}</li>
                 <li>{{artiste2}}</li>
                 <li>{{artiste3}}</li>
