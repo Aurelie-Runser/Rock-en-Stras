17f80cae80175bcb437b8355fa3558d6a7eacda1
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-06-01 01:42:29 +0200

    changement graphique

--- src/components/cardArtiste.vue
+++ src/components/cardArtiste.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="overflow-hidden aspect-video w-72 md:w-96 xl:w-[30rem] border-4 border-gray-300 skew-y-6">
-        <img class="-mt-5 md:-mt-9 w-[120%] object-cover center -skew-y-6" :src=image :alt=imgalt>
+    <div class="overflow-hidden aspect-video w-72 md:w-96 xl:w-[30rem] border-4 border-gray-300 skew-y-3">
+        <img class="-mt-7 w-full h-[130%] object-cover center -skew-y-3" :src=image :alt=imgalt>
     </div>
 </template>
 
