9e53aadf37e30a341af59b1f1c2153322d23b13d
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-05-31 18:26:35 +0200

    ajout du contenu personnalise

--- src/components/programArt.vue
+++ src/components/programArt.vue
@@ -5,14 +5,14 @@
         <cardPaysage class="hidden lg:block"/>
 
         <div class="text-white font-base sm:mx-auto">
-            <h4 class="font-semibold text-base sm:text-lg m-5 sm:m-10">Vendredi 15</h4>
+            <h4 class="font-semibold text-base sm:text-lg m-5 sm:m-10">{{jour}}</h4>
                         
             <ul class="font-semibold font-base text-lg sm:text-xl leading-10 sm:leading-[4rem] nth-child-2n:text-pink-500">
-                <li>Artiste 1</li>
-                <li>Artiste 2</li>
-                <li>Artiste 3</li>
-                <li>Artiste 4</li>
-                <li>Artiste 5</li>
+                <li>{{artiste1}}</li>
+                <li>{{artiste2}}</li>
+                <li>{{artiste3}}</li>
+                <li>{{artiste4}}</li>
+                <li>{{artiste5}}</li>
             </ul>
         </div>
     </div>
@@ -30,5 +30,26 @@ export default {
     };
   },
   components: { cardPortrait, cardPaysage },
+  
+  props:{
+    jour:{
+      type: String,
+    },
+     artiste1:{
+      type: String,
+     },
+     artiste2:{
+      type: String,
+     },
+     artiste3:{
+      type: String,
+     },
+     artiste4:{
+      type: String,
+     },
+     artiste5:{
+      type: String,
+     },
+ }
 } 
 </script>
