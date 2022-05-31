2db769cab988366cb8f6991a9dcf81bf07b30ab6
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-06-01 00:57:25 +0200

    creation de la page avec la grille

--- src/views/ArtistesView.vue
+++ src/views/ArtistesView.vue
@@ -1,5 +1,34 @@
 <template>
-    <div>
-        artistes
+    <div class="bg-pink-500 px-5 py-36 ">
+        <div class="m-auto max-w-7xl">
+            <h1 class="mb-12">Qui vient à Rock en Stras’ ?</h1>
+
+            <div class="mx-5">
+                <div class="max-w-5xl mx-auto
+                    grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(15rem,auto))] gap-10">
+
+                    <cardArtiste class="w-full md:w-full xl:w-full h-cal"/>
+                    <cardArtiste class="w-full md:w-full xl:w-full h-cal"/>
+                    <cardArtiste class="w-full md:w-full xl:w-full h-cal"/>
+                    <cardArtiste class="w-full md:w-full xl:w-full h-cal"/>
+                    <cardArtiste class="w-full md:w-full xl:w-full h-cal"/>
+                </div>
+            </div>
+        
+        </div>
     </div>
 </template>
+
+
+<script>
+import cardArtiste from "../components/cardArtiste.vue"
+
+export default {
+  name: "ProgramView",
+  data() {
+    return {
+    };
+  },
+  components: { cardArtiste },
+}
+</script>
