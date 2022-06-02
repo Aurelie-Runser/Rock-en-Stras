07a088bc63492e2d7c93db931acc838aced7be96
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-06-02 19:37:52 +0200

    ajout du bouton pour supprimer une liste back

---
+++ src/components/icons/deleteButton.vue
@@ -0,0 +1,6 @@
+<template>
+    <button class="w-14 h-14 hover:scale-110 focus:rotate-180 focus:scale-100
+                motion-safe:ease-in-out motion-safe:origin-center motion-safe:duration-300">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--mdi" width="auto" height="auto" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="#db2777" d="M12 2c5.53 0 10 4.47 10 10s-4.47 10-10 10S2 17.53 2 12S6.47 2 12 2m5 5h-2.5l-1-1h-3l-1 1H7v2h10V7M9 18h6a1 1 0 0 0 1-1v-7H8v7a1 1 0 0 0 1 1Z"></path></svg>        <span class="sr-only">Mettre à jour la liste</span>
+    </button>
+</template>
