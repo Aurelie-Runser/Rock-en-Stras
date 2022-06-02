1a640642bdf7b493e3f1137c686135660f959dc3
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-06-02 21:26:08 +0200

    ajout de la fonction ordeByNumber pour renger la liste dans l'ordre croissant + responsivité de la page ConcertView

--- src/views/ConcertView.vue
+++ src/views/ConcertView.vue
@@ -1,39 +1,41 @@
 <template>
     <div class="bg-base px-5 py-36 ">
-        <h1 class="mb-12">Concerts du ???</h1>
+        <h1 class="mb-12">Concerts du Vendredi 15 Avril</h1>
 
         <!--Scène 1-->
-        <div class="max-w-7xl mx-auto my-20">
-            <div class="flex flex-wrap justify-between items-center w-full sm:w-[80%] lg:w-full gap-x-5 lg:gap-x-10">
+        <div class="max-w-7xl mx-auto my-20 mb-40">
+            <div class="flex flex-wrap justify-between items-center w-full gap-x-5 lg:gap-x-10">
                 <div class="basis-12 flex-grow">
                     <h2>Scène 1</h2>
 
-                    <div class="my-5 flex gap-5"
+                    <div class="my-5 flex flex-wrap gap-5 w-auto items-center"
                          v-for="h in listeHorraire" :key="h.id">
                        <!-- <p class="text-pink-500">{{h.heure}}</p> -->
                        <!-- <p class="text-pink-500">{{h.groupe}}</p> -->
 
-                        <input class="h-11 w-60 m-0 px-5 py-0 bg-transparent border-transparent 
-                                        text-base lg:text-lg font-base font-semibold text-pink-500
-                                         motion-safe:transition motion-safe:duration-300
-                                        hover:border-white
-                                        focus:bg-gray-700 focus:text-gray-100" type='time' v-model='h.heure' />
+                        <div class="flex flex-wrap gap-5 items-center">
+                            <input class="h-11 w-auto m-0 px-5 py-0 bg-transparent border-transparent 
+                                            text-base sm:text-lg font-base font-semibold text-pink-500
+                                            motion-safe:transition motion-safe:duration-300
+                                            hover:border-white
+                                            focus:bg-gray-700 focus:text-gray-100" type='time' v-model='h.heure' />
 
-                        <input class="h-11 m-0 px-5 py-0 bg-transparent border-transparent 
-                                        text-base lg:text-lg font-base font-semibold text-pink-500
-                                         motion-safe:transition motion-safe:duration-300
-                                        hover:border-white
-                                        focus:bg-gray-700 focus:text-gray-100" type='text' v-model='h.groupe' />
-                        
-                        
+                            <input class="h-11 w-auto m-0 px-5 py-0 bg-transparent border-transparent 
+                                            text-base sm:text-lg font-base font-semibold text-pink-500
+                                            motion-safe:transition motion-safe:duration-300
+                                            hover:border-white
+                                            focus:bg-gray-700 focus:text-gray-100" type='text' v-model='h.groupe' />
+                        </div>
+
+                        <div class="flex gap-5 mx-auto">
                             <updateButton @click.prevent="updateHorraire(h)"/>
 
                             <deleteButton @click.prevent="deleteHorraire(h)"/>
-
+                        </div>
                     </div>
                 </div>
 
-                <card image="/scene1.jpg" imgalt="photo de la scène 1"/>
+                <card class="mx-auto md:aspect-[13/16] lg:aspect-video" image="/scene1.jpg" imgalt="photo de la scène 1"/>
 
             </div>
 
@@ -52,9 +54,8 @@
                     <h2>Scène 2</h2>
 
                     <ul class="font-semibold text-base sm:text-lg lg:text-xl leading-10 sm:leading-[4rem] lg:leading-[5rem] text-gray-100 nth-child-2n:text-pink">
-                        <li>14h - 16h :</li>
-                        <li>16h30h - 18h30 : </li>
-                        <li>19h - 22h :</li>
+                        <li>14h - 16h : Elmer Food Beat</li>
+                        <li>16h30h - 18h30 : Flagrants Délires</li>
                     </ul>
                 </div>
 
@@ -101,10 +102,21 @@ export default {
 
   data() {
     return {
+        heure: null,
         listeHorraire : []
     };
   },
 
+  computed:{
+      orderByTime:function(){
+        return this.listeHorraire.sort(function(a, b){
+          if(a.heure < b.heure) return -1;
+          if(a.heure > b.heure) return 1;
+          return 0
+        })
+      },
+  },
+  
   mounted(){
       // appel liste jour
       this.getHorraire();
