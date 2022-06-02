981f8867bc065ad757f78b2433705a2c0dd51136
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-06-02 15:45:31 +0200

    creation du bouton mettre a jour pour les updates des listes firebases

---
+++ src/components/icons/updateButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <button class="w-14 h-14 hover:scale-110 focus:rotate-180 focus:scale-100
+                motion-safe:ease-in-out motion-safe:origin-center motion-safe:duration-300">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--icon-park-solid" width="auto" height="auto" preserveAspectRatio="xMidYMid meet" viewBox="0 0 48 48"><mask id="IconifyId181247f8a9d792e8b67"><g fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"><path fill="#fff" stroke="#fff" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"></path><path stroke="#000" d="M33.542 27c-1.274 4.057-5.064 7-9.542 7c-4.477 0-8.268-2.943-9.542-7v6m19.084-18v6c-1.274-4.057-5.064-7-9.542-7c-4.477 0-8.268 2.943-9.542 7"></path></g></mask><path fill="#db2777" d="M0 0h48v48H0z" mask="url(#IconifyId181247f8a9d792e8b67)"></path></svg>
+        <span class="sr-only">Mettre à jour la liste</span>
+    </button>
+</template>
